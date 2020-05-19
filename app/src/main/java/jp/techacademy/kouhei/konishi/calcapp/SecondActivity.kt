9676695921b40f178c202701f524d54e751a2b99
konishikouhei72 <qbs0004@icloud.com>
2020-05-19 22:36:22 +0900

    提出課題作成中２_２

--- app/src/main/java/jp/techacademy/kouhei/konishi/calcapp/SecondActivity.kt
+++ app/src/main/java/jp/techacademy/kouhei/konishi/calcapp/SecondActivity.kt
@@ -2,6 +2,7 @@ package jp.techacademy.kouhei.konishi.calcapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_second.*
 
@@ -13,6 +14,23 @@ class SecondActivity : AppCompatActivity() {
 
         val value1 = intent.getDoubleExtra("VALUE1", 0.0)
         val value2 = intent.getDoubleExtra("VALUE2", 0.0)
-        textView.text = "${value1 + value2}"
+        val value3 = intent.getDoubleExtra("演算判定", 0.0)
+
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        alertDialogBuilder.setTitle("タイトル")
+        alertDialogBuilder.setMessage("メッセージ")
+
+        if (value3 == 1.0) {
+            textView.text = "${value1 + value2}"
+        }else if(value3 == 2.0){
+            textView.text = "${value1 - value2}"
+        }else if(value3 == 3.0){
+            textView.text = "${value1 * value2}"
+        }else if(value3 == 4.0){
+            textView.text = "${value1 / value2}"
+        }else if(value3 == 5.0){
+            // ここにダイアログでメッセージを出そうとしております
+        }
+
     }
 }
