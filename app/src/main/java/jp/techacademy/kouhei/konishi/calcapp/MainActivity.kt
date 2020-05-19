8f3124186ced6b929d452733beb52f74a9629c78
konishikouhei72 <qbs0004@icloud.com>
2020-05-19 16:50:46 +0900

    提出課題作成中２

--- app/src/main/java/jp/techacademy/kouhei/konishi/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/kouhei/konishi/calcapp/MainActivity.kt
@@ -7,16 +7,19 @@ import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+    var num1: Int = 0
+    var num2: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        button.setOnClickListener(this)
+        button1.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
         val intent = Intent(this, SecondActivity::class.java)
+
+
         intent.putExtra("VALUE1", 10)
         intent.putExtra("VALUE2", 20)
         startActivity(intent)
