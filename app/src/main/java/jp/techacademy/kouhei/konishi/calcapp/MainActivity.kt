d3cdbe488d83438d4a4dd0926b5870b14c1b5f1e
konishikouhei72 <qbs0004@icloud.com>
2020-05-19 18:04:21 +0900

    提出課題作成中２_1

--- app/src/main/java/jp/techacademy/kouhei/konishi/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/kouhei/konishi/calcapp/MainActivity.kt
@@ -5,23 +5,28 @@ import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
+import android.util.Log
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    var num1: Int = 0
-    var num2: Int = 0
+    var num1: Double = 0.0
+    var num2: Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         button1.setOnClickListener(this)
+        button2.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
         val intent = Intent(this, SecondActivity::class.java)
+        var num1 = editText1.text.toString().toDouble()
+        var num2 = editText2.text.toString().toDouble()
 
+        //Log.d("kotlintest", num1.toString().toDouble())
 
-        intent.putExtra("VALUE1", 10)
-        intent.putExtra("VALUE2", 20)
+        intent.putExtra("VALUE1", num1)
+        intent.putExtra("VALUE2", num2)
         startActivity(intent)
     }
 }
